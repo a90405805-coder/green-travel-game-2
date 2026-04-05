028852de279e7d65a35c26a70f7032c09c5aabf6
a90405805-coder <a90405805@gmail.com>
2026-04-06 01:18:59 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,14 @@
 const CACHE_NAME = 'green-planning-v2';
-// 列出所有需要離線使用的檔案
 const ASSETS = [
-  'index2.html',
+  'index.html',
   '1.png', '2.png', '3.png', '4.png',
   '1-1.png', '1-2.png', '1-3.png', '1-4.png', '1-5.png', '1-6.png', '1-7.png', '1-8.png', '1-9.png',
   '2-1.png', '2-2.png', '2-3.png', '2-4.png', '2-5.png',
   '3-1.png', '3-2.png',
   '4-1.png', '4-2.png', '4-3.png',
-  'success.mp3',
-  'error.mp3',
-  'applaud.mp3',
-  'itsOK.mp3',
+  'success.mp3', 'error.mp3', 'applaud.mp3', 'itsOK.mp3',
   'icon-512.png',
-  'manifest2.json'
+  'manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
